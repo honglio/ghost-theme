a7bf021f1ae206a5619628b497849f3d4ae69a38
honglio <hong.liu@live.cn>
2015-04-18 19:11:30 +0800

    clean up

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://www.niukj.com',
+        url: 'http://blog.niukj.com',
         mail: {},
         database: {
             client: 'mysql',
